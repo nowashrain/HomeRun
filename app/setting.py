bc021dd35d1fbfd43005444f57590c4162ce7492
nowashrain <137181048+nowashrain@users.noreply.github.com>
2024-09-25 16:31:17 +0900

    Update setting.py

--- app/setting.py
+++ app/setting.py
@@ -7,13 +7,9 @@ class Settings(BaseSettings):
     #dbname: str = 'ubuntu'
     dburl: str = 'localhost'
     #dbconn: str = f'sqlite:///app/{dbname}.db'
-     .env 파일을 읽도록 설정
+    dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
+    # .env 파일을 읽도록 설정
     class Config:
         env_file = ".env"
-
-    @property
-    def dbconn(self):
-        return f'mysql+pymysql://{self.userid}:{self.passwd}@{self.dburl}:3306/{self.dbname}?charset=utf8mb4'
-    
 
 config = Settings()
